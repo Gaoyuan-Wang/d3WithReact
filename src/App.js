89dccc21031c10a8d013091cecf512cd01e79e93
The winter of ink <wanggaouyuan@qq.com>
2024-02-08 14:35:39 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import * as d3 from "d3";
+import { useState } from "react";
+import Chart1 from "./LinePlot";
+
+export default function App() {
+  console.log("App");
+  return (
+    <div>
+      <Chart1/>
+    </div>
+  );
+}
