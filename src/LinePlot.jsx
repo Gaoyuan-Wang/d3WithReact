486edd7ba6c581c4d767394a1172c465bc0cfc4f
The winter of ink <wanggaouyuan@qq.com>
2024-02-08 18:58:26 +0800

    init

--- src/LinePlot.jsx
+++ src/LinePlot.jsx
@@ -2,7 +2,7 @@ import * as d3 from "d3";
 import { useState } from "react";
 
 async function loadData() {
-  var fileFath = "data/Avocado_HassAvocadoBoard_20152023v1.0.1.csv"
+  var fileFath = "data/test.csv"
   const data = await d3.csv(fileFath);
   return data
 }
@@ -41,75 +41,69 @@ function drawChart(data) {
   const gW = width - m.left;
   const gH = height - m.top - m.bottom;
 
-// 定义x坐标轴的比例尺，gW为x轴的宽度，关于scaleBand参考上一篇文章以及相关文章
-// 这里我们会得到一个xScale.bandwidth()的距离，bandwidth()访问每个波段的宽度：
- const xScale = d3.scaleBand().range([0, gW]);
+  // 定义x坐标轴的比例尺，gW为x轴的宽度，关于scaleBand参考上一篇文章以及相关文章
+  // 这里我们会得到一个xScale.bandwidth()的距离，bandwidth()访问每个波段的宽度：
+  const xScale = d3.scaleTime().range([0, gW]);
      
- // 定义好x轴定义域，画出x轴axisBottom，底部位置，year是值
-  xScale.domain(data.map(item => item.year));
+  // 定义好x轴定义域，画出x轴axisBottom，底部位置，year是值
+  xScale.domain(data.map(item => new Date(item.Date)));
  
   g.append("g")
-    .attr("transform", `translate(0, ${gH})`)
-    .call(d3.axisBottom(xScale))
-    .attr("stroke", "red");
+  .attr("transform", `translate(0, ${gH})`)
+  .call(d3.axisBottom(xScale))
+  .attr("stroke", "red");
     // 定义y坐标轴的比例尺，gH为y轴的宽度
- const yScale = d3.scaleLinear().range([gH, 0]);
+  const yScale = d3.scaleLinear().range([gH, 0]);
  
- // 定义好y轴d定义域，画出y轴，y轴画在左边axisLeft，value是值
+  // 定义好y轴d定义域，画出y轴，y轴画在左边axisLeft，value是值
  
- yScale.domain([0, d3.max(data, item => item.value)]);
+  yScale.domain([0, d3.max(data, item => item.AveragePrice)]);
  
- g.append("g")
-   .call(d3.axisLeft(yScale))
-   .attr("stroke", "red");
-   // 先给点画上小圆圈和文字，创建一个文字和圆圈的group
-// join那句可以改为以前v4写法.enter().append('circle')
+  g.append("g")
+  .call(d3.axisLeft(yScale))
+  .attr("stroke", "red");
+  // 先给点画上小圆圈和文字，创建一个文字和圆圈的group
+  // join那句可以改为以前v4写法.enter().append('circle')
 
-const group1 = g
-.selectAll(".gruop-circle-text")
-.data(data)
-.join("g")
-.attr("class", "gruop-circle-text");
+  const group1 = g.selectAll(".group-circle-text")
+  .data(data)
+  .join("g")
+  .attr("class", "group-circle-text");
 
-// 画出圆点即是圆圈，xScale.bandwidth()就用到了,bandwidth()访问每个波段的宽度,
-// xScale.bandwidth() / 2 我们把元素位置居中
+  // 画出圆点即是圆圈，xScale.bandwidth()就用到了,bandwidth()访问每个波段的宽度,
+  // xScale.bandwidth() / 2 我们把元素位置居中
 
- group1
-   .selectAll("circle")
-   .data(data)
-   .join("circle")
-   .attr("cx", d => {
-     return xScale(d.year) + xScale.bandwidth() / 2;
-   })
-   .attr("cy", d => {
-     return yScale(d.value);
-   })
-   .attr("r", 3)
-   .attr("fill", "red");
+  group1.selectAll("circle")
+  .data(data)
+  .join("circle")
+  .attr("cx", d => {
+    return xScale(new Date(d.Date));
+  })
+  .attr("cy", d => {
+    return yScale(d.AveragePrice);
+  }).attr("r", 3)
+  .attr("fill", "red");
    
    
   // 绘制出文字
 	
- group1
-   .selectAll("text")
-   .data(data)
-   .join("text")
-   .attr("x", d => {
-     return xScale(d.year) + xScale.bandwidth() / 2;
-   })
-   .attr("y", d => yScale(d.value) - 2)
-   .text(d => d.value);
-   // 创建一个line的生成器 用d3.line,把所有点连起来
-	const line = d3
-  .line()
-  .x(d => {
+  // group1.selectAll("text")
+  // .data(data)
+  // .join("text")
+  // .attr("x", d => {
+  //   return xScale(d.Date) + xScale.bandwidth() / 2;
+  // })
+  // .attr("y", d => yScale(d.AveragePrice) - 2)
+  // .text(d => d.AveragePrice);
+
+  // 创建一个line的生成器 用d3.line,把所有点连起来
+	const line = d3.line().x(d => {
     // 这里是d3.scaleBand自带比例尺
-    return xScale(d.year) + xScale.bandwidth() / 2;
-  })
-  .y(d => {
+    return xScale(new Date(d.Date));
+  }).y(d => {
     return yScale(d.value);
-  })
-  .curve(d3.curveCatmullRom);  //这里有多种形态可以选择
+  }).curve(d3.curveCatmullRom);  //这里有多种形态可以选择
+
   g.append("path")
   .attr("d", line(data))
   .attr("fill", "none")
@@ -122,14 +116,14 @@ export default function Chart1() {
   loadData().then((data) => {
     datas = data
     setState(JSON.stringify(data[0]));
-    console.log(data)
+    //console.log(data)
+    drawChart(data);
     return data[0];
   });
+  //drawChart(datas);
   return (
     <div>
       {dataset}
-      <svg width="800" height="400">
-      </svg>
     </div>
   )
 }
